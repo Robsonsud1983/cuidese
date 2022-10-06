067ede2f0d9c838bc0239255e311ee8d0e6e37c4
unknown <robson0@ba.estudante.senai.br>
2022-10-06 20:06:53 -0300

    Cuide-se

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <nav class="navbar navbar-expand-sm navbar-dark"  style="background-color: #26a008;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Cuide-se</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link active" aria-current="page" href="#">Início</a>
+              <a class="nav-link" href="#">Categorias</a>
+              <a class="nav-link" href="#">Login</a>
+              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Desativado</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    <title>Cuide-se</title>
+  </head>
+  <body>
+    <h1>Imagem com fundo verde</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>Carrosel</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h3>Maps</h3>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h3>footer</h3>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
